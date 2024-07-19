6117a6678940c3783ea790efbcb1fa458a0f5197
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-19 14:37:01 -0700

    add keyup event listner to push the pressed key value into pressed array; add splice method on the pressed array to only include the length of the secretcode at the end of the pressed array

---
+++ script.js
@@ -0,0 +1,9 @@
+const pressed = [];
+const secretCode = 'javascript';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    console.log(pressed);
+})
