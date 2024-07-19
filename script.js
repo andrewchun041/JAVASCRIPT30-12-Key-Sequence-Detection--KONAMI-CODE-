f567e6b9220f03898c4cd5f418504e56359a8b35
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-19 15:15:38 -0700

    add if statement to verify pressed array matches secretCode and console log the secretCode if it's a match

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const pressed = [];
 const secretCode = 'javascript';
 
 window.addEventListener('keyup', (e) => {
-    console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     console.log(pressed);
-})
+    if (pressed.join('').includes(secretCode)) {
+        console.log(`Secret code: ${secretCode}`);
+    }
+});
